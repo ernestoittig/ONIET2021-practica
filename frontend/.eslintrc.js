a69eda2b21ccec797e0a460b296d685ee8c2e406
Ernesto Ittig <e.ittig@gmail.com>
2021-10-02 22:46:23 -0300

    Added eslint and prettier

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,33 @@
+// const path = require('path');
+
+module.exports = {
+  env: {
+    node: true,
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+      parserOptions: {
+        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+      },
+    },
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
